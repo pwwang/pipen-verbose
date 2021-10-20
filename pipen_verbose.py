4c47e394041e5a35413ca43ecb50092bc86ee231
pwwang <pwwang@pwwang.com>
2021-10-20 16:01:13 -0700

    ⬆️ Upgrade pipen to 0.2+ in deps

--- pipen_verbose.py
+++ pipen_verbose.py
@@ -12,7 +12,7 @@ if TYPE_CHECKING:  # pragma: no cover
     from pipen import Proc
     from pipen.job import Job
 
-__version__ = "0.0.2"
+__version__ = "0.0.3"
 
 logger = get_logger("verbose", "info")
 
