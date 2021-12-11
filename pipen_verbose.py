783c7642a3bfb49603dc4532586428e75bb5a799
pwwang <pwwang@pwwang.com>
2021-12-10 22:21:43 -0700

    🔖 0.0.8

--- pipen_verbose.py
+++ pipen_verbose.py
@@ -13,7 +13,7 @@ if TYPE_CHECKING:  # pragma: no cover
     from pipen import Proc
     from pipen.job import Job
 
-__version__ = "0.0.7"
+__version__ = "0.0.8"
 
 logger = get_logger("verbose", "info")
 
