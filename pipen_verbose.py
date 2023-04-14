24e20d4e95e001e300bc82432c63d71d074b31e4
pwwang <pwwang@pwwang.com>
2023-04-14 00:05:38 -0700

    🔖 0.6.0

--- pipen_verbose.py
+++ pipen_verbose.py
@@ -12,7 +12,7 @@ from pipen.utils import get_logger, brief_list
 if TYPE_CHECKING:  # pragma: no cover
     from pipen import Proc
 
-__version__ = "0.5.0"
+__version__ = "0.6.0"
 
 logger = get_logger("verbose", "info")
 
