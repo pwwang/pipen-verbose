958e8df5702b29f4570ebe454677ef68be1fbe82
justold <1188067+pwwang@users.noreply.github.com>
2023-10-19 21:41:00 +0000

    🔖 0.9.0

--- pipen_verbose.py
+++ pipen_verbose.py
@@ -12,7 +12,7 @@ from pipen.utils import get_logger, brief_list
 if TYPE_CHECKING:  # pragma: no cover
     from pipen import Proc
 
-__version__ = "0.8.1"
+__version__ = "0.9.0"
 
 logger = get_logger("verbose", "info")
 
