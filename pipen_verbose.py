e10057fda38583862808294d3d17b53dfe7da75f
pwwang <pwwang@pwwang.com>
2025-04-18 20:18:50 -0700

    0.14.1

--- pipen_verbose.py
+++ pipen_verbose.py
@@ -17,7 +17,7 @@ from pipen.utils import get_logger, brief_list, logger_console
 if TYPE_CHECKING:  # pragma: no cover
     from pipen import Proc
 
-__version__ = "0.14.0"
+__version__ = "0.14.1"
 
 logger = get_logger("verbose", "info")
 T = TypeVar("T", list, tuple, set)
